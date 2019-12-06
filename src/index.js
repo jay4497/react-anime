e2d44e4ecbe6b61c3ae5d70385f281039e398110
jay4497 <jay4497@126.com>
2019-12-06 17:59:59 +0800

    init

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Button from '@material-ui/core/Button'
+import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            animes: []
+        };
+    }
+    componentDidMount() {
+        fetch('https://api.jikan.moe/v3/season/2019/fall')
+            .then((response) => {
+                return response.json()
+            })
+            .then((animeList) => {
+                this.setState({animes: animeList.anime})
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    render() {
+        return (
+            <div class="container">
+            {this.state.animes.map((anime) => {
+                return (
+                    <div class="card">
+                        <h1>{anime.title}({anime.type})</h1>
+                        <img src={anime.image_url}/>
+                        <p>{anime.synopsis}</p>
+                    </div>
+                    )
+                })}
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector('#app'))
